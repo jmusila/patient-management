885def0c3b543d5645d87f22e3253e948976c550
jmusila <jonathanmusila6@gmail.com>
2023-11-21 10:21:40 +0300

    model doctors table

---
+++ database/migrations/2023_11_21_071827_create_doctors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->string('license_number');
+            $table->string('specialization');
+            $table->date('date_of_hire');
+            $table->string('title');
+            $table->integer('experience_years');
+            $table->text('short_bio')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
