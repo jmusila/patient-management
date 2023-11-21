79484bc7c42366de56aeea156b25b86bd358e17a
jmusila <jonathanmusila6@gmail.com>
2023-11-21 10:39:27 +0300

    added necessary enums

---
+++ app/Enums/Roles.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+class Roles
+{
+    public const ADMIN = "Admin";
+
+    public const DOCTOR = "Doctor";
+
+    public const RECEPTIONIST = "Receptionist";
+}
---
+++ app/Enums/Titles.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+enum Titles: string
+{
+    case PROF = "Prof";
+    case DR = "Dr";
+    case MR = "Mr";
+    case MRS = "Mrs";
+    case MISS = "Miss";
+    case MS = "Ms";
+    case REV = "Rev";
+}
